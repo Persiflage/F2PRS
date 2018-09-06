905f3f62946e1dfe72f31af9535fd07049805d9d
pacocoursey <pvco.coursey@gmail.com>
2018-09-06 13:46:41 -0600

    update homepage

--- index.php
+++ index.php
@@ -13,27 +13,6 @@
 
 
 		<p style="width: 480px; margin: 30px auto; line-height: 24px;">
-			<span style="color: #53E053">Feb 12</span>/ <a class="gain" href="tools/rates/">EHP rates</a> have been updated.</p>
-
-		<p style="width: 480px; margin: 30px auto; line-height: 24px;">
-			<span style="color: #53E053">Recent</span>/ Various tools are available <a class="gain" href="tools/">here</a>.</p>
-
-		<br>
-
-
-		<h3>Changelog</h3>
-		<div style="width: 480px; margin: auto;">
-			<p>
-				Prayer rate changed to 610k<br>
-				Crafting rate reverted to 630k<br>
-				Firemaking is now lossless from 11.7m xp onwards<br>
-				Dungeoneering rate changed to 290k
-			</p>
-
-			<p>Please see <a href="https://www.youtube.com/watch?v=tJ-OC16PBBM" class="gain">this video</a> for an example of how Prayer should be efficiently trained.</p>
-		</div>
-
-		<br/><br/>
-
+			<span style="color: #53E053">Sep 6</span>/ <a class="gain" href="tools/rates/">EHP rates</a> have been updated.</p>
 	</body>
 </html>
