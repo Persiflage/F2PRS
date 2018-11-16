df5382fef124c06695d9aee1291c950f9b1b8165
pacocoursey <pvco.coursey@gmail.com>
2018-11-15 19:38:18 -0700

    update announcement on home page

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<h1 style="display: inline-block">Welcome.</h1><p style="width: 480px; margin: 0 auto; line-height: 24px;"> F2PRS was created so that XP focused F2P players could find accurate hiscores. The minimalistic page layout is designed so that you can access desired information quickly and easily.</p>
 
 		<p style="width: 480px; margin: 30px auto; line-height: 24px;">
-			<span style="color: #53E053">Sep 6</span>/ <a class="gain" href="tools/rates/">EHP rates</a> have been updated.
+			<span style="color: #53E053">Nov 15</span>/ <a class="gain" href="tools/rates/">Ranged EHP</a> has been updated.
 			<br>
 			<span style="color: #53E053">Sep 10</span>/ Various tools can be found <a class="gain" href="tools/">here.</a>
 		</p>
