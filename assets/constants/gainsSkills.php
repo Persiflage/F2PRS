144702ee38089e5818656f87759a0c002f4c8618
pacocoursey <pvco.coursey@gmail.com>
2019-01-24 23:16:45 -0700

    add history states for each skill tab in gains

---
+++ assets/constants/gainsSkills.php
@@ -0,0 +1,62 @@
+<?php
+    $gainsSkills = array(
+        array(
+            "name" => "total",
+            "sql_key" => "total_xp"
+        ), array(
+            "name" => "attack",
+            "sql_key" => "attack_xp"
+        ), array(
+            "name" => "defence",
+            "sql_key" => "defence_xp"
+        ), array(
+            "name" => "strength",
+            "sql_key" => "strength_xp"
+        ), array(
+            "name" => "constitution",
+            "sql_key" => "constitution_xp"
+        ), array(
+            "name" => "ranged",
+            "sql_key" => "ranged_xp"
+        ), array(
+            "name" => "prayer",
+            "sql_key" => "prayer_xp"
+        ), array(
+            "name" => "magic",
+            "sql_key" => "magic_xp"
+        ), array(
+            "name" => "cooking",
+            "sql_key" => "cooking_xp"
+        ), array(
+            "name" => "woodcutting",
+            "sql_key" => "woodcutting_xp"
+        ), array(
+            "name" => "fletching",
+            "sql_key" => "fletching_xp"
+        ), array(
+            "name" => "fishing",
+            "sql_key" => "fishing_xp"
+        ), array(
+            "name" => "firemaking",
+            "sql_key" => "firemaking_xp"
+        ), array(
+            "name" => "crafting",
+            "sql_key" => "crafting_xp"
+        ), array(
+            "name" => "smithing",
+            "sql_key" => "smithing_xp"
+        ), array(
+            "name" => "mining",
+            "sql_key" => "mining_xp"
+        ), array(
+            "name" => "runecrafting",
+            "sql_key" => "runecrafting_xp"
+        ), array(
+            "name" => "dungeoneering",
+            "sql_key" => "dungeoneering_xp"
+        ), array(
+            "name" => "ehp",
+            "sql_key" => "ehp"
+        )
+    );
+?>
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -10,14 +10,42 @@ $(document).ready(function() {
 });
 
 jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
+	// Are we on gains.php? Is there a hash value in URL? Is there valid
+	// selector element?
+	if (window.location.pathname === '/gains.php') {
+		var hash = window.location.hash && window.location.hash.slice(1); // Remove the `#`
+		var $selector = jQuery('#tab-selector-' + hash);
+		if ($selector.length) {
+			$selector.addClass('active');
+			jQuery('#tab-' + hash).addClass('active');
+		} else {
+			// Set the first tab as active
+			jQuery('#tab-selector-total').addClass('active');
+			jQuery('#tab-total').addClass('active');
+		}
+	}
 
-        // Show/Hide Tabs
-		jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+	jQuery('.tabs .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
 
-        e.preventDefault();
-    });
+		if (window.location.hash !== currentAttrValue) {
+			// Show/Hide Tabs
+			var skillName = currentAttrValue.slice(1);
+			jQuery('#tab-' + skillName).fadeIn(400).siblings().hide();
+			// Change/remove current tab to active
+			jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+			history.pushState(null, null, window.location.pathname + '#' + skillName);
+		}
+		e.preventDefault();
+	});
+
+	window.addEventListener('popstate', function(e) {
+		var hash = e.target && e.target.window && e.target.window.location.hash && e.target.window.location.hash.slice(1);
+
+		// Show/Hide Tabs
+		jQuery('#tab-' + hash).fadeIn(400).siblings().hide();
+		// Change/remove current tab to active
+		jQuery('#tab-selector-' + hash).addClass('active').siblings().removeClass('active');
+	});
 });
